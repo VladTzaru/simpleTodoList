805badc76a4a66bf8ee1508b2787a3537c5abf31
VladTzaru <bvanja.87@gmail.com>
2018-03-12 18:43:34 +0100

    add new toDo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,46 @@
 $(function() {
 	"use strict";
+
+	init();
+
+	function init() {
+		toggleDone();
+		removeTodo();
+	}
     
 
 	// Do / Undo a toDo
-    $("li").on("click", function() {
-    	$(this).toggleClass("done");
-    });
+	function toggleDone() {
+		$("ul").on("click", "li", function() {
+	    	$(this).toggleClass("done");
+	    });
+	}
 
 
-    // Remove a toDo
-    $("span").on("click", function(e) {
-    	e.stopPropagation();
-    	$(this).parent().fadeOut("1000", "linear", function() {
-    		$(this).remove();
-    	});
+	function removeTodo() {
+	    $("ul").on("click", "span", function(e) {
+	    	$(this).parent().fadeOut("1000", "linear", function() {
+	    		$(this).remove();
+	    	});
+	    	e.stopPropagation();
+	    });
+	}
+
+
+    // Add new toDo
+    $(".input-toDo").keypress(function(e) {
+    	if (e.which === 13) {
+    		let inputVal = $(this).val();
+    		$("ul").append(`<li><span>X</span> ${inputVal}</li>`);
+    		// Clear input
+    		inputVal = null;
+
+    	}
     });
+
+    $(".btn-addTodo").on("click", function() {
+    	alert("bang");
+    })
 
 
 
